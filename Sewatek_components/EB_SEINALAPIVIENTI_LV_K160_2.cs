e00d4e2ffb030363ee967ef44cdd0ec3830ba761
gustg <gustg@users.noreply.github.com>
2014-08-16 19:54:44 -0500

    Update EB_SEINALAPIVIENTI_LV_K160_2.cs
    Material field changed.

--- Sewatek_components/EB_SEINALAPIVIENTI_LV_K160_2.cs
+++ Sewatek_components/EB_SEINALAPIVIENTI_LV_K160_2.cs
@@ -166,6 +166,8 @@ namespace Sewatek_components
                 _AspreAttribut1 = _Data.P6a;
             else
                 _AspreAttribut1 = "EB_LV_K160_2";
+                
+            _MaterialAttribute = "Misc_undefined";
 
             if (_Data.UDAn1 != String.Empty && _Data.UDAv1 != String.Empty)
             {
